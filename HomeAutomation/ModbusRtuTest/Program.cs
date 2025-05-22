91e8531f79a14ff999d48c760420dfa4683c3e46
stanko.culaja <stanko.culaja.extern@adviqo.com>
2025-05-22 16:45:06 +0200

    Added ModbsuRtuTest application with simple command sending to the slave.

---
+++ HomeAutomation/ModbusRtuTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Ports;
+using NModbus;
+using NModbus.Serial;
+
+using var port = new SerialPort("/dev/ttyUSB0");
+port.BaudRate = 9600;
+port.Parity = Parity.None;
+port.DataBits = 8;
+port.StopBits = StopBits.One;
+port.ReadTimeout = 1000;
+port.WriteTimeout = 1000;
+port.Open();
+
+var master = new ModbusFactory().CreateRtuMaster(port);
+
+master.WriteMultipleRegisters(
+    slaveAddress: 1,
+    startAddress: 100,
+    data: [1, 2, 3]);
